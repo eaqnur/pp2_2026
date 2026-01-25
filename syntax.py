13e758a9faa2d13f0810a67db2a3c81c3c5b56bd
Aknur <a_erezhep@kbtu.kz>
2026-01-25 15:27:12 +0500

    python basics

---
+++ syntax.py
@@ -0,0 +1,17 @@
+a=5
+b=2
+if a>b:
+    print("a is greater than b")
+
+'''This is a multi line comment'''
+if 9>3:
+    print("Nine is greater than three")
+
+#syntax_error
+'''if 4<6:
+print("Six is greater than four")
+
+if 7>5:
+    print("Seven is greater than five")
+           print("Seven is greater than five")
+           '''
